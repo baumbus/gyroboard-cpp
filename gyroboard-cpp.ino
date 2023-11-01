d2cbce944c1cf380eebbd8cfdebe7ce7f69c1153
baumbus <baumbus_github@icloud.com>
2023-11-01 20:10:13 +0100

    testing lcd i2c library

--- gyroboard-cpp.ino
+++ gyroboard-cpp.ino
@@ -1,8 +1,18 @@
-void setup() {
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup()
+{
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello, world!");
     Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
     Serial.println("Hello World!");
     delay(1000);
 }
